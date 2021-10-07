b33caca12f95779c8adc93e8576a961376d6341f
KristjanPet <91966438+KristjanPet@users.noreply.github.com>
2021-10-07 13:36:48 +0200

    Popravlejno
    Mankal ti je en presledek

--- ClapSensor.ino
+++ ClapSensor.ino
@@ -1,21 +1,16 @@
 #define MicSamples (1024*2)
 #define MicPin A1
- bool relay = false; 
+bool relay = false; 
 
 // measure basic properties of the input signal
 // determine if analog or digital, determine range and average.
 void setup(){
-  Serial.begin(9600);
- digitalWrite(13, HIGH);
-  
-  }
+	Serial.begin(9600);
+	digitalWrite(13, HIGH);
+}
 void loop(){
-
-{
-  pinMode(13, OUTPUT);
-  
-
-        long signalAvg = 0, signalMax = 0, signalMin = 1024, t0 = millis();
+	pinMode(13, OUTPUT);
+    long signalAvg = 0, signalMax = 0, signalMin = 1024, t0 = millis();
     for (int i = 0; i < MicSamples; i++)
     {
         int k = analogRead(MicPin);
@@ -24,29 +19,25 @@ void loop(){
         signalAvg += k;
     }
     signalAvg /= MicSamples;
-      
- 
-    // print
+    
     Serial.println(signalMax - signalAvg);
     if((signalMax - signalAvg)>12){
       if(relay == false){
-      relay = true;
-      Serial.println("Vklop");
-      delay(1000);
+		relay = true;
+		Serial.println("Vklop");
+		delay(1000);
       }
       else{
-      relay = false;
-      Serial.println("Izklop");
-      delay(1000);
+		relay = false;
+		Serial.println("Izklop");
+		delay(1000);
       }
-      }
+    }
     if(relay == true){
       digitalWrite(13, HIGH);
       
-      }
-    if(relay == false){
+    }
+    else if(relay == false){
       digitalWrite(13, LOW);
-      
-      }
- 
-}}
+    }
+}
